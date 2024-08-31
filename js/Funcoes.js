7b8520d6b784c48dd22d30748145ef605390e7d0
Rafael <113179737+rafaelpkz@users.noreply.github.com>
2024-08-31 12:07:56 -0300

    Loja-1
    Loja feita através das aula do 1 semestre da faculdade.

---
+++ js/Funcoes.js
@@ -0,0 +1,14 @@
+function adicionar(produto){
+
+    const qtd = document.getElementsById('Qtd_' + Produto)
+    const valor = document.getElementsById('Valor_' + Produto)
+    const total = document.getElementsById('Total_' + Produto)
+
+    qtd.innerHTML ++
+    const resultado = qtd.innerHTML * valor.innerHTML
+
+    total.innerHTML = resultado
+
+
+
+}
